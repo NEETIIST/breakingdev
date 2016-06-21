cdbf2758a37d3d6992cd2c06e73950e307723862
filiperfernandes <filipe.ramalhofernandes@gmail.com>
2016-06-21 21:13:03 +0100

    Add contact form

--- breakingdev/breakingdev.js
+++ breakingdev/breakingdev.js
@@ -1,6 +1,8 @@
 
 devers = new Meteor.Collection("devers");
 
+
+
 if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
@@ -26,10 +28,42 @@ Template.devReg.events({
     
      // Clear form
     event.target.name.value = "";
-    event.target.age.value = "";
-    event.target.city.value = "";
+    event.target.mail.value = "";
+    event.target.subject.value = "";
+    event.target.data.value = "";
 
   }
+
+});
+
+Template.contactUs.events({
+
+  'submit .contactUsForm' : function (event) {
+
+    event.preventDefault();
+    
+
+    var name = event.target.name.value;
+    var mail = event.target.mail.value;
+    var subject = event.target.subject.value;
+    var data = event.target.data.value;
+
+
+    // In your client code: asynchronously send an email
+    Meteor.call('sendEmail',
+            'geral@neeti.tecnico.ulisboa.pt',
+            mail,
+            subject,
+            data);
+
+    // Clear form
+    event.target.name.value = "";
+    event.target.mail.value = "";
+    event.target.subject.value = "";
+    event.target.data.value = "";
+
+  }
+
 
 });
 
@@ -38,8 +72,33 @@ Template.devReg.events({
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
+
+
+    //replace xxxxx and yyyyy with gmail username and password
+    process.env.MAIL_URL = "smtp://xxxxx%40gmail.com:yyyyyyy@smtp.gmail.com:465/"
+
+
+Meteor.methods({
+  sendEmail: function (to, from, subject, text) {
+    check([to, from, subject, text], [String]);
+
+    // Let other method calls from the same client start running,
+    // without waiting for the email sending to complete.
+    this.unblock();
+
+    Email.send({
+      to: to,
+      from: from,
+      subject: subject,
+      text: text
+    });
+  }
+});
+
   });
 }
+
+
 
 // Iron Router
 
